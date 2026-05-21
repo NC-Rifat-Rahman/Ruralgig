fe7df94a6b0e13aa5b8654fac859f39e1581155e
NC-Rifat-Rahman <rahmanrifat61@gmail.com>
2026-05-21 17:14:14 +0600

    feat(auth-service): auth service and controller user register initial implementation.

--- apps/auth-service/src/users/users.controller.ts
+++ apps/auth-service/src/users/users.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
-@Controller()
+@Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) { }
 
-    @Get()
-    getHello(): string {
-        return "";
+    @Post('register')
+    async register(dto: CreateUserDto) {
+        try {
+            await this.usersService.register(dto);
+        } catch (error) {
+            throw error;
+        }
     }
 }
--- apps/auth-service/src/users/users.service.ts
+++ apps/auth-service/src/users/users.service.ts
@@ -1,10 +1,14 @@
 
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersRepository } from './users.repository';
 
 export type User = any;
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) { }
   private readonly users = [
     {
       userId: 1,
@@ -17,6 +21,22 @@ export class UsersService {
       password: 'guess',
     },
   ];
+
+  async register(dto: CreateUserDto): Promise<User> {
+    const { email, password } = dto;
+
+    const existingUser = await this.usersRepository.findOne(dto);
+
+    if (existingUser) {
+      throw new BadRequestException('User already exists');
+    }
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = await this.usersRepository.create(dto, hashedPassword);
+
+    return newUser;
+  }
 
   async findOne(username: string): Promise<User | undefined> {
     return this.users.find(user => user.username === username);
